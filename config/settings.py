897b2e783256ad19251067a997e04d9ee3683680
BolarinwaSheu30 <bolarinwasheu42@gmail.com>
2026-04-28 22:19:02 +0100

    fix production settings

--- config/settings.py
+++ config/settings.py
@@ -24,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)r7b82xf10)2)l9%flmx*l)#gr@6+8u$g1+e*@d0%d(_b4wmva'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = ['*'] # Allow all
 
